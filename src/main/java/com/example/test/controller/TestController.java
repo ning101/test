a2c48ee42693f54e6610aeb8891c59d4c7985133
downing <193278572@qq.com>
2022-08-19 14:20:34 +0800

    Merge branch 'test1'
    # Conflicts:
    #	src/main/java/com/example/test/controller/TestController.java

--- src/main/java/com/example/test/controller/TestController.java
+++ src/main/java/com/example/test/controller/TestController.java
@@ -1,10 +1,4 @@
 package com.example.test.controller;
 
 public class TestController {
-    public static void main(String[] args) {
-        System.out.println("aa");
-        System.out.println("bb");
-        System.out.println("cc");
-        System.out.println("dd");
-    }
 }
