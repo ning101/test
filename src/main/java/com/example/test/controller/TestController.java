8ee5547e10986dd213d20d1aceea4e1d621c9cf0
downing <193278572@qq.com>
2022-08-19 14:16:20 +0800

    test6

--- src/main/java/com/example/test/controller/TestController.java
+++ src/main/java/com/example/test/controller/TestController.java
@@ -3,5 +3,6 @@ package com.example.test.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("aa");
+        System.out.println("bb");
     }
 }
